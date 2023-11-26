ba52f8b04a5331fcfc4efece532c0681b2998472
unknown <rpranay14@gmail.com>
2023-11-26 12:01:08 +0530

    added netlify url in cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-    'http://localhost:3000', 'http://127.0.0.1:3000', 'http://192.168.29.25:3000', 'https://64a27b5bde745155387bdc75--scintillating-croquembouche-6f6f8f.netlify.app/', 'https://scintillating-croquembouche-6f6f8f.netlify.app'
+    'http://localhost:3000','https://delightful-paprenjak-036812.netlify.app', 'http://127.0.0.1:3000', 'http://192.168.29.25:3000', 'https://64a27b5bde745155387bdc75--scintillating-croquembouche-6f6f8f.netlify.app/', 'https://scintillating-croquembouche-6f6f8f.netlify.app'
 
 ]
 
